55ca1504b68b2faf017c8dde4d20ac84e146973d
Vikas Bansal <bansalvikas2626@gmail.com>
2023-01-14 22:33:35 +0530

    added admin dashboard

--- youtube/admin.py
+++ youtube/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from youtube import models
 
-# Register your models here.
+@admin.register(models.YoutubeVideos)
+class YoutubeVideosAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'search_tag',
+        'description',
+        'published_at',
+        'thumbnail_data',
+        'video_id'
+    ]
+    list_filter = ['title', 'search_tag']
