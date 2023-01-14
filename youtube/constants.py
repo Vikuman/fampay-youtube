d9337c27424760981a805575e7ea014fac56f8ca
Vikas Bansal <bansalvikas2626@gmail.com>
2023-01-15 01:44:30 +0530

    changed choices format

--- youtube/constants.py
+++ youtube/constants.py
@@ -11,13 +11,13 @@ class SearchTag(enum.Enum):
 
     @classmethod
     def choices(cls):
-        return tuple((i.name, i.value) for i in cls)
+        return tuple((i.value, i.name) for i in cls)
 
 class Platform(enum.Enum):
     YOUTUBE = 1
 
     @classmethod
     def choices(cls):
-        return tuple((i.name, i.value) for i in cls)
+        return tuple((i.value, i.name) for i in cls)
 
 YOUTUBE_REDIS_API_KEY_NAME = 'youtube_redis_api_key_name'
